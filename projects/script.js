f2ec6066491564b2becafcb918949b48796fecd4
Thi-mee <timswaggs1@outlook.com>
2022-04-13 18:10:58 +0100

    nil

--- projects/script.js
+++ projects/script.js
@@ -1,63 +1,145 @@
+const root = document.documentElement;
+const checkbox = document.getElementById('toggle');
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+const WINNING_COMBINATIONS = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('game-board')
+const winningMessageElement = document.getElementById('winning-message')
+const restartButton = document.getElementById('restart-button')
+const winningMessageTextElement = document.querySelector('[data-winning-text-element]')
+let oTurn
+let evaluation
 
 
-const computerBtn = document.getElementById('computer-button')
-const friendBtn = document.getElementById('friend-button')
-const nextBtn = document.getElementById('next-button')
-const xBtn = document.getElementById('x-button')
-const oBtn = document.getElementById('o-button')
-const playBtn = document.getElementById('play-button')
-const firstScreen = document.querySelector(".first-screen")
-const secondScreen = document.querySelector(".second-screen")
-const gameBoard = document.querySelector(".game-board")
-const player = new Object
-let opponent
+restartButton.addEventListener('click', startGame)
 
-
-
-
-computerBtn.addEventListener('click', () =>{
-  resetButtonColor(computerBtn,friendBtn)
-  switchActive(friendBtn,computerBtn)
-  opponent = "Computer AI"
-})
-friendBtn.addEventListener('click', () =>{
-  resetButtonColor(computerBtn,friendBtn)
-  switchActive(computerBtn,friendBtn)
-  opponent = "Friend"
-})
-nextBtn.addEventListener('click', () =>{
-  if(!opponent){
-    computerBtn.style.backgroundColor = "rgb(97, 0, 0)"
-    friendBtn.style.backgroundColor = "rgb(97, 0, 0)"
-    return
+function startGame(){ 
+  if(player.you == "X"){
+  oTurn  = false
   }
-  firstScreen.classList.remove("show")
-  secondScreen.classList.add("show")
-})
-xBtn.addEventListener('click', () =>{
-  switchActive(oBtn,xBtn)
-  player.you = "X"
-  if(opponent === "Computer AI")player.computer = "O"
-  else if(opponent === "Friend")player.friend = "O"
-})
-oBtn.addEventListener('click', () =>{
-  switchActive(xBtn,oBtn)
-  player.you = "X"
-  if(opponent === "Friend")player.computer = "X"
-  else if(opponent === "Computer AI")player.friend = "X"
-})
-playBtn.addEventListener('click', () => {
-  secondScreen.classList.remove("show")
-  gameBoard.classList.add("show")
-})
+  else if (player.you =="O"){oTurn = true}
+  setBoardHoverClass()
 
+  cellElements.forEach(cell => {
+  cell.classList.remove(X_CLASS)
+  cell.classList.remove(O_CLASS)
+  cell.removeEventListener('click', handleClick)
 
-
-function switchActive(off,on){
-  off.classList.remove("active")
-  on.classList.add("active")
+  cell.addEventListener('click', handleClick,{once: true})
+  winningMessageElement.classList.remove('show')
+});
 }
-function resetButtonColor(x,y) {
-  x.style.backgroundColor = "#18383F"
-  y.style.backgroundColor = "#18383F"
+
+
+
+
+
+function handleClick(e){
+  const cell = e.target
+  const currentClass = oTurn ? O_CLASS : X_CLASS
+
+  // placemark
+    placeMark(cell, currentClass)
+  if(opponent === "Friend"){
+      // check for win or draw
+    if(checkWin(currentClass)){
+      endGame(false)
+    }
+    else if(isDraw()){
+      endGame(true)
+    }
+    else{
+      // switchturns
+      swapTurns()
+      setBoardHoverClass()
+    }
+  }
+  else if(opponent === "Computer AI"){
+    const emptySpaces = []
+    
+    if(checkWin(currentClass)){
+      setBoardHoverClass()
+      endGame(false)
+    }
+    else if(isDraw()){
+      endGame(true)
+    }
+    minimax(cellElements, player)
+
+    
+
+
+  }
+  
+}
+
+
+
+
+function placeMark(cell, currentClass){
+  cell.classList.add(currentClass)
+}
+function swapTurns(){
+  oTurn = !oTurn
+}
+function setBoardHoverClass(){
+  board.classList.remove(X_CLASS)
+  board.classList.remove(O_CLASS)
+  if(oTurn){
+    board.classList.add(O_CLASS)
+  }
+  else{
+    board.classList.add(X_CLASS)
+  }
+} 
+function checkWin(currentClass){
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+function isDraw(){
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS)
+  })
+}
+function endGame(draw){
+  if(draw){
+    winningMessageTextElement.innerText = "Draw!"
+  }
+  else{
+    winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Win!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function getEmptySpaces(array,emptyArray){
+  array.forEach(element => {
+    if(element.classList.contains('x')||element.classList.contains('o')){
+      return
+    }
+    else{
+      emptyArray.push(element)
+    }
+  });
+}
+
+function minimax(cellElements, player){
+  //BASE
+  if(isWinner(cellElements, player.computer)) return(evaluation = +10);
+  if(isWinner(cellElements, player.you)) return (evaluation = -10);
+  if(isDraw()) return (evaluation = 0);
+
+  //Look for empty spaces
+  getEmptySpaces(cellElements, emptySpaces)
+
+  //save all moves and their evaluation
+  let moves
+
 }
